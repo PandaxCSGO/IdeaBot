a46797de159823586ae21a1216f29a57830d7842
PandaxCSGO <31318425+PandaxCSGO@users.noreply.github.com>
2020-05-20 14:27:47 +0200

    Major crash fix
    Fixed crash when a person had quotes marks in their nickname

--- index.js
+++ index.js
@@ -52,11 +52,12 @@ client.on(`ready`, () => {
 // Coeur du bot
 client.on(`message`, message => {
   const dmchannel = client.channels.get(config.idchannel);
-  const msgcontent = message.content.replace(/"/gi, `”`); // si nous ne faisons pas ça alors le bot crash si qql met `Mon idée c'est " blablabla`
-   const msgcontent2 = msgcontent.replace(/`/gi, `”`);
+  const ideacontent = message.content.replace(/"/gi, `”`); // si nous ne faisons pas ça alors le bot crash si qql met `Mon idée c'est " blablabla`
+  const ideacontent2 = ideacontent.replace(/`/gi, `”`); // si nous ne faisons pas ça alors le bot crash si qql met `Mon idée c'est " blablabla`
+  const pseudo = message.author.username.replace(/"/gi, `”`);
   var validation_time_ms = Math.round(config.validation_time * 1000)
 
-  let sql = `INSERT INTO `+config.table+` (`+config.colone_pseudo+`, `+config.colone_idea+`, `+config.colone_date+`) VALUES ("`+message.author.username+`", "`+msgcontent2+`", "`+timestamp+`")`;
+  let sql = `INSERT INTO `+config.table+` (`+config.colone_pseudo+`, `+config.colone_idea+`, `+config.colone_date+`) VALUES ("`+pseudo+`", "`+ideacontent2+`", "`+timestamp+`")`;
 
   if (message.channel.type === "dm") {
       if (message.author.id === client.user.id) return;
@@ -66,11 +67,10 @@ client.on(`message`, message => {
             message.channel.awaitMessages(response => (response.content == `oui` || response.content == `OUI` || response.content == `Oui` || response.content == `OUi` || response.content == `OuI` || response.content == `oUI` || response.content == `oUi` || response.content == `ouI`), {
             max: 1,
             time: validation_time_ms,
-            errors: ['Oui'],
           })
         .then((collected) => {
-            message.channel.send(`Votre idée à été envoyer (${collected.first().content}):\n\`\`\`${msgcontent2}\`\`\``);
-            dmchannel.send(`Idée reçue de ${message.author}, Merci de voter via `+config.reaction1+` ou `+config.reaction2+`.\n\`\`\`${msgcontent2}\`\`\`\n\n`).then(function (message) {
+            message.channel.send(`Votre idée à été envoyer (${collected.first().content}):\n\`\`\`${ideacontent2}\`\`\``);
+            dmchannel.send(`Idée reçue de ${message.author}, Merci de voter via `+config.reaction1+` ou `+config.reaction2+`.\n\`\`\`${ideacontent2}\`\`\`\n\n`).then(function (message) {
               message.react(config.reaction1);
               setTimeout(function() {message.react(config.reaction2);}, 2000);
             }
